b01ff6a5a07648ceb9379ceed5dbf4bf95c9a136
Rudra gauswami <rudrapuri.g.reliution@gmail.com>
2026-01-29 12:25:50 +0530

    aadd

--- Task_1/models/sale_ord_line.py
+++ Task_1/models/sale_ord_line.py
@@ -6,6 +6,7 @@ class Sale(models.Model):
 
     shutter_height = fields.Float('Shutter Height')
     shutter_length = fields.Float('Shutter Length')
+    compute_shutter_area = fields.Float(compute='_compute_compute_shutter_area',string='Shutter Area',store=True)
 
     @api.constrains('shutter_height', 'shutter_length', 'product_id')
     def _check_apron_shutter_dimensions(self):
@@ -24,3 +25,8 @@ class Sale(models.Model):
                        ''' Selected Apron Shutter product has dimension limits.
                            Please reset Height or Length within allowed range.'''
                     )
+
+    @api.depends('shutter_height','shutter_length')
+    def _compute_compute_shutter_area(self):
+        for line in self:
+            line.compute_shutter_area = line.shutter_height * line.shutter_length
