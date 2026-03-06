a7771b4498027311252808d143f098d11f6149e6
Rudra gauswami <rudrapuri.g.reliution@gmail.com>
2026-03-06 17:12:49 +0530

    add

--- mrp_production_priority_task/models/mrp_prod.py
+++ mrp_production_priority_task/models/mrp_prod.py
@@ -4,11 +4,21 @@ class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     priority = fields.Selection(selection_add=[('2', 'Medium Priority'),
-                                               ('3', 'High Priority')],)
+                                               ('3', 'High Priority')],
+                                ondelete={
+                                    '2': 'set default',
+                                    '3': 'set default'
+                                }
+                                )
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
     priority = fields.Selection(selection_add=[('2', 'Medium Priority'),
-                                               ('3', 'High Priority')], )
+                                               ('3', 'High Priority')],
+                                ondelete={
+                                    '2': 'set default',
+                                    '3': 'set default'
+                                }
+                                )
